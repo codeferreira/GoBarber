45208a799597c9b95aae8c397e02cf6f5ee9529f
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-06-04 18:28:59 -0300

    Routes
    - Home/Signin
    - Signup
    - Dashboard

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
+const routes = express.Router()
+
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
+
+module.exports = routes
