41492812a9e94e21e0efbd2ca92aa93702d6b7c9
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-06-14 18:02:26 -0300

    Added
    - Auth Middleware
    - Guest Middleware
    - DashboardController
    - FileController
    - AppointmentController
    - AvaibleDateController
    - Flash messages
    - Files Route
    - Appointments Routes
    - Availeble Date Route
    - apply Middlewares to routes

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,38 @@ const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 const routes = express.Router()
 
+const authMiddleware = require('./app/middleware/auth')
+const guestMiddleware = require('./app/middleware/guest')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
+const AvailableController = require('./app/controllers/AvailableController')
 
-routes.get('/', SessionController.create)
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+
+  return next()
+})
+
+routes.get('/files/:file', FileController.show)
+
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.get('/app/logout', SessionController.destroy)
+
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
-routes.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
-  res.render('dashboard')
-})
+routes.use('/app', authMiddleware)
+
+routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
