d49e5589ed8f4793a13506cdc34e45ab944eed39
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-09-13 21:12:47 -0300

    Software prints

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   dialect: 'postgres',
   host: '127.0.0.1',
-  username: 'docker',
+  username: 'postgres',
   password: 'docker',
   database: 'gobarber',
   operatorAliases: false,
